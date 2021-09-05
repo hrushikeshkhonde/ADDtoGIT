54186d2edb6a17e3ad0a2d383236691adc9c3496
Hrushikesh Khonde <hrushikeshkhonde57@gmail.com>
2021-09-05 12:34:26 +0530

    Add stat.

--- add.java
+++ add.java
@@ -1,6 +1,7 @@
 public class add {
     public static void main(String args[])
     {
-        System.out.print("Hello Git...")
+        System.out.print("Hello Git...");
+        System.out.print("Task Complete !");
     }
 }
