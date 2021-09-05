6d4e467b050748b574c27e405866b476dbeb3e8f
Hrushikesh Khonde <hrushikeshkhonde57@gmail.com>
2021-09-05 12:46:31 +0530

    NEW

---
+++ Temp.java
@@ -0,0 +1,12 @@
+public class Temp
+{
+    public static void main(Sting args[])
+    {
+     double c,f;
+     c=45;
+     f=1.8*c+32.00;
+     System.out.print("Temperature in fareinheit= "+f);
+     System.out.print("Task Complete !");
+
+    }
+}
